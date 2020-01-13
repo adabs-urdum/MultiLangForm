3ed66e9b5e994f4faf797470e8e0f23118973b24
Adabs Urdum <cyrill@adabs.ch>
2020-01-13 20:28:10 +0100

    added some explainatory comments to messages file

--- MultiLangFormMessages.php
+++ MultiLangFormMessages.php
@@ -11,21 +11,25 @@ $message['error'] = [
 	'en' => 'An error occured, please try again later.'
 ];
 
+//Bei Input Fehlern / Wenn die Validierung scheitert
 $message['problem'] = [
 	'de' => 'Hier stimmt was nicht.',
-	'en' => 'Something is wrong here.'
+	'en' => 'Something went wrong here.'
 ];
 
+//E-Mail Betreff
 $message['subject'] = [
 	'de' => 'Nachricht deiner Website.',
 	'en' => 'Your website has called.'
 ];
 
+//E-Mail Body vor der Tabelle
 $message['emailMessage'] = [
 	'de' => '<h1>TEST</h1><p>Vielen Dank für die Blumen. Vielen Dank, wie lieb von dir.</p>',
 	'en' => '<h1>TEST EN</h1><p>Thank you for the flowers. Thank, that\'s very kind of you.</p>',
 ];
 
+//E-Mail Footer
 $message['autoMessage'] = [
 	'de' => '<hr/><p>Das ist ein Automatisch generiertes Mail. Bitte nicht auf dieses Mail antworten!</p>',
 	'en' => '<hr/><p>This is an automatically generated e-mail. Please do not reply to this e-mail!</p>',
